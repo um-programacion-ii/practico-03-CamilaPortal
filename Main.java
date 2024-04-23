137bf73040bfbd6551853f0bf036f10ffcc29be6
CamilaPortal <c.portal@alumno.um.edu.ar>
2024-04-23 17:27:20 -0300

    correccion en la implementacion de hilos. Codigo optimizado

--- Main.java
+++ Main.java
@@ -4,7 +4,9 @@ import entity.*;
 import service.CocinaService;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -12,9 +14,10 @@ public class Main {
     public static void main(String[] args) throws StockInsuficiente, VidaUtilInsuficiente {
         List<Ingrediente> ingredientes = new ArrayList<>();
         ingredientes.add(new Ingrediente("Agua", 500));
-        ingredientes.add(new Ingrediente("Huevos", 6));
+        ingredientes.add(new Ingrediente("Huevo", 100));
         ingredientes.add(new Ingrediente("Lechuga", 6));
         ingredientes.add(new Ingrediente("Tomate", 6));
+        ingredientes.add(new Ingrediente("Manzana", 6));
         ingredientes.add(new Ingrediente("Aceite de oliva", 500));
 
         List<Utensilio> utensilios = new ArrayList<>();
@@ -22,32 +25,25 @@ public class Main {
         utensilios.add(new Utensilio("Olla", 10));
         utensilios.add(new Utensilio("Bol", 10));
 
-        Despensa despensa1 = new Despensa();
-        Despensa despensa2 = new Despensa();
-        Despensa despensa3 = new Despensa();
-        Despensa despensa4 = new Despensa();
-        Despensa despensa5 = new Despensa();
-        Despensa despensa6 = new Despensa();
-        Despensa despensa7 = new Despensa();
-        Despensa despensa8 = new Despensa();
 
-        agregarIngredientesUtensilios(despensa1, new ArrayList<>(ingredientes), new ArrayList<>(utensilios));
-        agregarIngredientesUtensilios(despensa2, new ArrayList<>(ingredientes), new ArrayList<>(utensilios));
-        agregarIngredientesUtensilios(despensa3, new ArrayList<>(ingredientes), new ArrayList<>(utensilios));
-        agregarIngredientesUtensilios(despensa4, new ArrayList<>(ingredientes), new ArrayList<>(utensilios));
-        agregarIngredientesUtensilios(despensa5, new ArrayList<>(ingredientes), new ArrayList<>(utensilios));
-        agregarIngredientesUtensilios(despensa6, new ArrayList<>(ingredientes), new ArrayList<>(utensilios));
-        agregarIngredientesUtensilios(despensa7, new ArrayList<>(ingredientes), new ArrayList<>(utensilios));
-        agregarIngredientesUtensilios(despensa8, new ArrayList<>(ingredientes), new ArrayList<>(utensilios));
+        ArrayList<Despensa> despensas = new ArrayList<>();
 
-        Chef chef1 = new Chef("Juan", 1, despensa1);
-        Chef chef2 = new Chef("Pepe", 2, despensa2);
-        Chef chef3 = new Chef("Luis", 3, despensa3);
-        Chef chef4 = new Chef("Ana", 4, despensa4);
-        Chef chef5 = new Chef("Maria", 5, despensa5);
-        Chef chef6 = new Chef("Pedro", 1, despensa6);
-        Chef chef7 = new Chef("Carlos", 4, despensa7);
-        Chef chef8 = new Chef("Sofia", 3, despensa8);
+        for (int i = 0; i < 8; i++) {
+            despensas.add(new Despensa());
+        }
+
+        for (int i = 0; i < despensas.size(); i++) {
+            agregarIngredientesUtensilios(despensas, i, new ArrayList<>(ingredientes), new ArrayList<>(utensilios));
+        }
+
+        Chef chef1 = new Chef("Juan", 1, despensas.get(0));
+        Chef chef2 = new Chef("Pepe", 2, despensas.get(1));
+        Chef chef3 = new Chef("Luis", 3, despensas.get(2));
+        Chef chef4 = new Chef("Ana", 4, despensas.get(3));
+        Chef chef5 = new Chef("Maria", 5, despensas.get(4));
+        Chef chef6 = new Chef("Pedro", 1, despensas.get(5));
+        Chef chef7 = new Chef("Carlos", 4, despensas.get(6));
+        Chef chef8 = new Chef("Sofia", 3, despensas.get(7));
 
         List<Chef> chefsDomingoAJueves = new ArrayList<>();
         List<Chef> chefsFinDeSemanaFeriados = new ArrayList<>();
@@ -66,54 +62,43 @@ public class Main {
         Ensalada ensalada = new Ensalada();
         EnsaladaDeFrutas ensaladaDeFrutas = new EnsaladaDeFrutas();
 
-//        try {
-//            CocinaService.prepararReceta(ensalada, chef1, despensa1.getIngredientes(), despensa1.getUtensilios());
-//        } catch (StockInsuficiente e) {
-//            System.out.println("Error: " + e.getMessage());
-//        }catch (VidaUtilInsuficiente e) {
-//            System.out.println("Error de vida útil insuficiente: " + e.getMessage());
-//        }
-//
-//        try {
-//            CocinaService.prepararReceta(ensalada, chef2, despensa2.getIngredientes(), despensa2.getUtensilios());
-//        } catch (StockInsuficiente e) {
-//            System.out.println("Error: " + e.getMessage());
-//        }catch (VidaUtilInsuficiente e) {
-//            System.out.println("Error de vida útil insuficiente: " + e.getMessage());
-//        }
-
         ExecutorService domingoAJuevesService = Executors.newFixedThreadPool(3);
         ExecutorService findesYFeriadosService = Executors.newFixedThreadPool(5);
 
-        domingoAJuevesService.submit(() -> {
-            try {
-                CocinaService.prepararReceta(huevoDuro, chef1, despensa1.getIngredientes(), despensa1.getUtensilios());
-            } catch (StockInsuficiente | VidaUtilInsuficiente e) {
-                System.out.println("Error en la receta de HuevoDuro: " + e.getMessage());
-            }
-        });
+        for (Chef chef : chefsDomingoAJueves) {
+            domingoAJuevesService.execute(() -> {
+                try {
+                    CocinaService.prepararReceta(obtenerRecetaAleatoria(), chef, chef.getDespensa().getIngredientes(), chef.getDespensa().getUtensilios());
+                } catch (StockInsuficiente e) {
+                    System.out.println("Error: " + e.getMessage());
+                } catch (VidaUtilInsuficiente e) {
+                    System.out.println("Error de vida útil insuficiente: " + e.getMessage());
+                }
+            });
+        }
 
-        domingoAJuevesService.submit(() -> {
-            try {
-                CocinaService.prepararReceta(ensalada, chef2, despensa2.getIngredientes(), despensa2.getUtensilios());
-            } catch (StockInsuficiente | VidaUtilInsuficiente e) {
-                System.out.println("Error en la receta de Ensalada: " + e.getMessage());
-            }
-        });
-
-        domingoAJuevesService.submit(() -> {
-            try {
-                CocinaService.prepararReceta(ensaladaDeFrutas, chef3, despensa3.getIngredientes(), despensa3.getUtensilios());
-            } catch (StockInsuficiente | VidaUtilInsuficiente e) {
-                System.out.println("Error en la receta de EnsaladaDeFrutas: " + e.getMessage());
-            }
-        });
-
+        for (Chef chef : chefsFinDeSemanaFeriados) {
+            findesYFeriadosService.execute(() -> {
+                try {
+                    CocinaService.prepararReceta(obtenerRecetaAleatoria(), chef, chef.getDespensa().getIngredientes(), chef.getDespensa().getUtensilios());
+                } catch (StockInsuficiente e) {
+                    System.out.println("Error: " + e.getMessage());
+                } catch (VidaUtilInsuficiente e) {
+                    System.out.println("Error de vida útil insuficiente: " + e.getMessage());
+                }
+            });
+        }
         domingoAJuevesService.shutdown();
+        findesYFeriadosService.shutdown();
     }
+    private static Receta obtenerRecetaAleatoria() {
+        List<Receta> recetasDisponibles = Arrays.asList(new Ensalada(), new HuevoDuro(), new EnsaladaDeFrutas());
+        Random random = new Random();
+        return recetasDisponibles.get(random.nextInt(recetasDisponibles.size()));
+    }
+    public static void agregarIngredientesUtensilios(List<Despensa> despensas, int indiceDespensa, List<Ingrediente> ingredientes, List<Utensilio> utensilios) {
+        Despensa despensa = despensas.get(indiceDespensa);
 
-
-    public static void agregarIngredientesUtensilios(Despensa despensa, List<Ingrediente> ingredientes, List<Utensilio> utensilios) {
         for (Ingrediente ingrediente : ingredientes) {
             Ingrediente nuevoIngrediente = new Ingrediente(ingrediente.getNombre(), ingrediente.getCantidad());
             despensa.addIngrediente(nuevoIngrediente);
@@ -124,5 +109,6 @@ public class Main {
             despensa.addUtensilio(nuevoUtensilio);
         }
     }
+
 
 }
